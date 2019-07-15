daa841fcaadc440721b44962fe4710efe72a6f5b
Krists Krilovs <krists.krilovs@gmail.com>
2019-07-15 13:54:26 -0700

    Bypass APP_ID check.

--- RSBypass/RSBypass/RSBypass.m
+++ RSBypass/RSBypass/RSBypass.m
@@ -11,13 +11,6 @@ typedef int8_t  BYTE;
 const DWORD  segStart = 0x001000;
 const size_t segLen   = 0x013ce000;
 
-// Could be made more general to bypass all signature checks
-// including ini files...
-// For the time being only patches the sig verif of sng files
-//const char*  hint     = "\x0f\x84\xc6\x0a\x00\x00";
-const char*  hint     = "\x84\xdb\x0f\x84\xde\x0a\x00\x00";
-
-
 bool bCompare(const BYTE* pData, const BYTE * bMask, const char* szMask) {
     for (; *szMask; ++szMask, ++pData, ++bMask)
         if (*szMask == 'x' && *pData != *bMask)
@@ -36,16 +29,22 @@ void* FindPattern(DWORD dwAddress, size_t dwLen, BYTE* bMask, char* szMask) {
 @implementation RSBypass
 +(void)load
 {
+    void* ptr;
+    long page_size;
+    void* page_start;
+
 //    NSLog(@"RSBypass: load()");
 
-    void* ptr = FindPattern(segStart, segLen,
-                            (BYTE*)hint, "xxxxxxxx");
+    page_size = sysconf(_SC_PAGESIZE);
+
+    ptr = FindPattern(segStart, segLen,
+        (BYTE*)"\x84\xdb\x0f\x84\xde\x0a\x00\x00",
+        "xxxxxxxx");
+
     if (ptr) {
+        page_start = (long)ptr & -page_size;
         
-        long page_size = sysconf(_SC_PAGESIZE);
-        void* page_start = (long)ptr & -page_size;
-        
-//        NSLog(@"Removing memory protection");
+        // Removing memory protection
         mprotect(page_start, page_size, PROT_READ | PROT_WRITE | PROT_EXEC);
         
 //        NSLog(@"RSInjector: Bypassing signature check at %p", ptr);
@@ -54,7 +53,24 @@ void* FindPattern(DWORD dwAddress, size_t dwLen, BYTE* bMask, char* szMask) {
         
         // Restoring memory protection
         mprotect(page_start, page_size, PROT_READ | PROT_EXEC);
+    }
+
+    ptr = FindPattern(segStart, segLen,
+        (BYTE*)"\x81\xff\xf0\x61\x03\x00\x75\x6f",
+        "xxxxxxxx");
+
+    if (ptr) {
+        page_start = (long)ptr & -page_size;
         
+        // Removing memory protection
+        mprotect(page_start, page_size, PROT_READ | PROT_WRITE | PROT_EXEC);
+        
+//        NSLog(@"RSInjector: Bypassing APP_ID check at %p", ptr);
+        memset(ptr, 0x90, 8);
+        msync(ptr, 8, MS_SYNC);
+        
+        // Restoring memory protection
+        mprotect(page_start, page_size, PROT_READ | PROT_EXEC);
     }
 }
 @end
